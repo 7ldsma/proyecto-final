eee859b25871b731318a457cfde4c9180338b05f
7ldsma <alvaro.ledesmav@gmail.com>
2022-12-12 15:55:08 +0100

    project set up

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    Dashboard
+    <RouterView/>
+</template>
+
+<script>
+import {mapStores} from 'pinia'
+import userStore from '../stores/user.js'
+export default {
+
+    computed:{
+    ...mapStores(userStore)
+},
+
+methods: {
+    login (){
+        this.userStore.signUp()
+    }
+}
+}
+
+
+</script>
