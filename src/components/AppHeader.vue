80331367b4441596034f692a2b182bd22cf4a935
7ldsma <alvaro.ledesmav@gmail.com>
2022-12-16 10:14:56 +0100

    se agrego Logout button y user display

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,14 +2,36 @@
     <header>
         <div class="flex flex-row justify-between">
             <img src="../assets/logo.png" alt="" width="70">
-            <div>{{user}}</div>
+            <div>
+                <p v-if="userStore.user">Hello, {{this.userStore.user.email}}</p>
+            </div>
+            <div>
+                <button v-if="userStore.user" @click="logout">Log-out</button>
+            </div>
             <img src="../assets/Menuicon.png" alt="" width="50">
         </div>
     </header>
 </template>
 
 <script>
+
+import {mapStores} from 'pinia'
+import userStore from '../stores/user.js'
+
     export default {
-        props : ["user"]
-    };
+
+        computed:{
+    ...mapStores(userStore)
+    },
+
+    methods: {
+
+        logout (){
+            this.userStore.user = null
+            this.$router.push('/')
+        },
+
+    }
+
+    }
 </script>
